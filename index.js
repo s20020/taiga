449d4ed9fdeb06a9c57de48c328d7f8f0cc30dbc
s20020 <s20020@std.it-college.ac.jp>
2022-02-24 12:28:15 +0900

    サーバ作成完了

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const port = 3000,
+    http = require("http"),
+    httpStatus = require("http-status-codes"),
+    app = http.createServer((req, res) => {
+        res.writeHead(httpStatus.OK, {
+            "Content-Type" : "text/html"
+        });
+        let responseMessage = "<h1>Welcome!</h1>";
+        res.end(responseMessage);
+    })
+.listen(port);
